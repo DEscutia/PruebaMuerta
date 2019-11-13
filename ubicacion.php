6a5d4632fcc064ffa890ba82889892d5d0788a57
DEscutia <44597771+DEscutia@users.noreply.github.com>
2019-11-12 19:58:01 -0600

    add cosas

--- ubicacion.php
+++ ubicacion.php
@@ -1,3 +1,20 @@
 <section>
    <a name="ubicacion"><h3 class="h3 text-center mb-5">Ubicación</h3></a> 
+   <div class="row">
+      <div class="col-md-6">
+         <div id="map-container" class="map-container z-depth-1-half" 
+            style="height: 400px;">
+            <iframe
+               height=500
+               width =500
+               frameborder="0" style="border:0"
+               src="https://www.google.com/maps/embed/v1/view?key=AIzaSyCzsLS8zFEb5D0vpDtdJ_Gk17DooR7Mnzo
+               &center=20.1311721,-101.1909645&zoom=18" allowfullscreen>
+            </iframe>            
+         </div>
+      </div> <!-- Col-->
+      <div class="col-md-6">
+         <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium aperiam, nostrum natus maxime ut esse et fuga quis, perferendis voluptatum cum fugiat dolores enim ipsa at, quod modi corrupti eveniet?</p>
+      </div>
+   </div>
 </section>
